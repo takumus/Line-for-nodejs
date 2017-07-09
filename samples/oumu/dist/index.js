b86c29e8192e6caae79771438f18bab31250e248
takumus <me@takumus.com>
2017-07-09 19:14:19 +0900

    おうむ返し修正

--- samples/oumu/dist/index.js
+++ samples/oumu/dist/index.js
@@ -1,17 +1,11 @@
 'use strict';
 exports.__esModule = true;
 var Config = require('../config');
-var _1 = require("../../../libs/");
-var line = new _1.Line(Config.channelSecret, // シークレット
+var LINE = require("../../../libs/");
+var line = new LINE.Connector(Config.channelSecret, // シークレット
 Config.channelAccessToken, // アクセストークン
 Config.serverPort // 自分のwebhockのhttpsサーバーのポート
 );
-// なんらかのデータがLINEから来た時
-line.on('data', function (e) {
-});
-// WebhookEvent[]を切り出してイベントを数回呼ばれるように。
-line.on('event', function (e) {
-});
 // メッセージが来た時。
 line.on('message', function (message, replyToken, event) {
     console.log(event.source.userId);
--- samples/oumu/src/index.ts
+++ samples/oumu/src/index.ts
@@ -1,25 +1,16 @@
 'use strict';
 const Config = require('../config');
-import { Line, LineData, LineEvent, LineMessage, LineProfile } from '../../../libs/';
+import * as LINE from '../../../libs/';
 
-const line = new Line(
+const line = new LINE.Connector(
     Config.channelSecret, // シークレット
     Config.channelAccessToken, // アクセストークン
     Config.serverPort // 自分のwebhockのhttpsサーバーのポート
 );
 
-// なんらかのデータがLINEから来た時
-line.on('data', (e: LineData) => {
-});
-
-// WebhookEvent[]を切り出してイベントを数回呼ばれるように。
-line.on('event', (e: LineEvent) => {
-});
-
 // メッセージが来た時。
-line.on('message', (message: LineMessage, replyToken: string, event: LineEvent) => {
-    console.log(event.source.userId);
-    line.getProfile(event.source.userId).then((profile: LineProfile) => {
+line.on('message', (message: LINE.Message, replyToken: string, event: LINE.Event) => {
+    line.getProfile(event.source.userId).then((profile: LINE.Profile) => {
         console.log(profile);
         message.text += '\nby ' + profile.displayName;
         line.reply(replyToken, [message]);
